47aa0ae20a0a4d7d2c99ca13c559cb9bc55709ac
huhu <760866069@qq.com>
2025-04-21 21:23:37 +0800

    nacos - 注册中心 - 服务注册

---
+++ services/service-order/src/main/java/com/hugo/order/OrderMainApplication.java
@@ -0,0 +1,13 @@
+package com.hugo.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMainApplication.class, args);
+    }
+
+}
