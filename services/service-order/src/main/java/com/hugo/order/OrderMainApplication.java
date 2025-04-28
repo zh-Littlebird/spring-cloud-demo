4af8d26f60e67bbc6ad30c1234212988f948f5e9
huhu <760866069@qq.com>
2025-04-28 19:38:06 +0800

    nacos - 配置中心 - 配置监听

--- services/service-order/src/main/java/com/hugo/order/OrderMainApplication.java
+++ services/service-order/src/main/java/com/hugo/order/OrderMainApplication.java
@@ -1,7 +1,16 @@
 package com.hugo.order;
 
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class OrderMainApplication {
@@ -10,4 +19,27 @@ public class OrderMainApplication {
         SpringApplication.run(OrderMainApplication.class, args);
     }
 
+    // 1、项目启动就监听配置文件变化
+    // 2、发生变化后拿到变化值
+    // 3、发送邮件
+    @Bean
+    public ApplicationRunner applicationRunner(NacosConfigManager nacosConfigManager) {
+        return args -> {
+            ConfigService configService = nacosConfigManager.getConfigService();
+            configService.addListener("service-order.properties",
+                    "DEFAULT_GROUP",
+                    new Listener() {
+                        @Override
+                        public Executor getExecutor() {
+                            return Executors.newFixedThreadPool(4);
+                        }
+
+                        @Override
+                        public void receiveConfigInfo(String s) {
+                            System.out.println("配置文件发生变化了，新的值是：" + s);
+                        }
+                    });
+            System.out.println("============");
+        };
+    }
 }
